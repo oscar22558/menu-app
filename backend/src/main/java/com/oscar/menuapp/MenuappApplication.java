e1979f25f772723a4b54662865dddfe61a542a0b
Oscar Shum <oscar22558oscar@gmail.com>
2022-11-06 17:22:15 +0800

    set up project

---
+++ backend/src/main/java/com/oscar/menuapp/MenuappApplication.java
@@ -0,0 +1,13 @@
+package com.oscar.menuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/oscar/menuapp/MenuappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscar.menuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
